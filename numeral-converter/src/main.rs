47d4b021f5686b495b344cf22af3fa7555c9a804
Igor Lubura <igocigonogedigo@gmail.com>
2022-10-16 15:06:23 +0200

    errors, don't use this yet

--- numeral-converter/src/main.rs
+++ numeral-converter/src/main.rs
@@ -1,15 +1,23 @@
 use std::io;
+use std::process;
 
 fn main() {
     println!("A numeral system converter");
     converter();
-    let mut number = String::new();
-    io::stdin().read_line(&mut number).expect("Error reading");
-    println!("{}",number);
 }
 
 fn converter() {
     let mut number = String::new();
+    let mut ns = String::new();
+    println!("Please enter a number: ");
     io::stdin().read_line(&mut number).expect("Error reading");
-    println!("{}",number);
+    println!("To what numeral system do you want to convert? (binary - B, octal - O, hexadecimal - H; quit - q)");
+    io::stdin().read_line(&mut ns).expect("Error reading");
+    if ns == "q" {
+        process::exit(0);
+    }
+    else if ns == "b" {
+        print!("HI");
+    };
+    return converter()
 }
---
+++ tempCodeRunnerFile.go
@@ -0,0 +1,1 @@
+"A numeral system converter"
