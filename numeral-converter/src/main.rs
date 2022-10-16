565e940c5a26a942982062cc32b2de08fc3431d4
Igor Lubura <igocigonogedigo@gmail.com>
2022-10-16 13:45:29 +0200

    basic structure

--- numeral-converter/src/main.rs
+++ numeral-converter/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    
+    println!("A numeral system converter");
+    converter();
+    let mut number = String::new();
+    io::stdin().read_line(&mut number).expect("Error reading");
+    println!("{}",number);
+}
+
+fn converter() {
+    let mut number = String::new();
+    io::stdin().read_line(&mut number).expect("Error reading");
+    println!("{}",number);
 }
